e12a0fa96a886f526d93019b439a19b47cbc139f
karino <karino@karinovista>
2012-08-16 22:54:28 +0900

    check-in for changing machine.

--- src/com/livejournal/karino2/calibrationtest/DrawingCanvas.java
+++ src/com/livejournal/karino2/calibrationtest/DrawingCanvas.java
@@ -1,12 +1,47 @@
 package com.livejournal.karino2.calibrationtest;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.view.View;
 
 public class DrawingCanvas extends View {
 
+    private Bitmap  mBitmap;
+    private Canvas  mCanvas;
+    private Path    mPath;
+    private Paint   mBitmapPaint;
+    private Paint       mPaint;
+
+
 	public DrawingCanvas(Context context) {
 		super(context);
+        mPath = new Path();
+        mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+        
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setColor(0xFFFF0000);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(12);
+
 	}
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+    }
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(0xFFAAAAAA);
+
+        canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
+
+        canvas.drawPath(mPath, mPaint);
+    }
 
 }
