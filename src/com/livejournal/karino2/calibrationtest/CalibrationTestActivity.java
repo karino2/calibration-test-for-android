53fec3affe86a855513f0c24dd5241cf99ad206c
karino <karino@karinovista>
2012-08-16 22:27:57 +0900

    initial commit. not yet buildable.

---
+++ src/com/livejournal/karino2/calibrationtest/CalibrationTestActivity.java
@@ -0,0 +1,13 @@
+package com.livejournal.karino2.calibrationtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalibrationTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ src/com/livejournal/karino2/calibrationtest/DrawingCanvas.java
@@ -0,0 +1,12 @@
+package com.livejournal.karino2.calibrationtest;
+
+import android.content.Context;
+import android.view.View;
+
+public class DrawingCanvas extends View {
+
+	public DrawingCanvas(Context context) {
+		super(context);
+	}
+
+}
